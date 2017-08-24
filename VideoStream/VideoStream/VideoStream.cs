87c42d25e841152b20c47bde0e311ccead1ea94b
pawit <triplepai.14@gmail.com>
2017-08-24 14:00:22 +0700

    Initial project

---
+++ VideoStream/VideoStream/VideoStream.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Web;
+using System.Web.Http;
+
+namespace VideoStreamLib
+{
+    public class VideoStream
+    {
+        #region Fields
+
+        // This will be used in copying input stream to output stream.
+        public const int ReadStreamBufferSize = 1024 * 1024;
+
+        public static readonly IReadOnlyDictionary<string, string> MimeNames;
+
+
+        #endregion
+
+
+        #region Constructors
+
+        static VideoStream()
+        {
+            var mimeNames = new Dictionary<string, string>();
+
+            mimeNames.Add(".mp3", "audio/mpeg");    // List all supported media types; 
+            mimeNames.Add(".mp4", "video/mp4");
+            mimeNames.Add(".ogg", "application/ogg");
+            mimeNames.Add(".ogv", "video/ogg");
+            mimeNames.Add(".oga", "audio/ogg");
+            mimeNames.Add(".wav", "audio/x-wav");
+            mimeNames.Add(".webm", "video/webm");
+
+            MimeNames = new ReadOnlyDictionary<string, string>(mimeNames);
+
+
+
+        }
+
+        #endregion
+        public HttpResponseMessage StreamVideo(string filePath, RangeHeaderValue rangeHeader, int chunkFileSizeMB = 0)
+        {
+            // This can prevent some unnecessary accesses. 
+            // These kind of file names won't be existing at all. 
+            if (string.IsNullOrWhiteSpace(filePath))
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            FileInfo fileInfo = new FileInfo(filePath);
+
+            if (!fileInfo.Exists)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            long totalLength = fileInfo.Length;
+
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            response.Headers.AcceptRanges.Add("bytes");
+
+            // The request will be treated as normal request if there is no Range header.
+            if (rangeHeader == null || !rangeHeader.Ranges.Any())
+            {
+                response.StatusCode = HttpStatusCode.OK;
+                response.Content = new PushStreamContent((outputStream, httpContent, transpContext)
+                =>
+                {
+                    using (outputStream) // Copy the file to output stream straightforward. 
+                    {
+                        using (Stream inputStream = fileInfo.OpenRead())
+                        {
+                            try
+                            {
+                                inputStream.CopyTo(outputStream, ReadStreamBufferSize);
+                            }
+                            catch (Exception error)
+                            {
+                                throw new Exception(error.ToString());
+                            }
+                        }
+                    }
+
+                }, GetMimeNameFromExt(fileInfo.Extension));
+
+                response.Content.Headers.ContentLength = totalLength;
+                return response;
+            }
+            long start = 0, end = 0;
+
+            // 1. If the unit is not 'bytes'.
+            // 2. If there are multiple ranges in header value.
+            // 3. If start or end position is greater than file length.
+            if (rangeHeader.Unit != "bytes" || rangeHeader.Ranges.Count > 1 ||
+                !TryReadRangeItem(rangeHeader.Ranges.First(), totalLength, out start, out end))
+            {
+                response.StatusCode = HttpStatusCode.RequestedRangeNotSatisfiable;
+                response.Content = new StreamContent(Stream.Null);  // No content for this status.
+                response.Content.Headers.ContentRange = new ContentRangeHeaderValue(totalLength);
+                response.Content.Headers.ContentType = GetMimeNameFromExt(fileInfo.Extension);
+
+                return response;
+            }
+
+
+            // Adjust length of chunk file size
+            if (chunkFileSizeMB > 0)
+            {
+                if (start + chunkFileSizeMB * 1024 * 1024 > totalLength - 1)
+                {
+                    end = totalLength - 1;
+                }
+                else
+                {
+                    end = start + chunkFileSizeMB * 1024 * 1024;
+                }
+            }
+
+            var contentRange = new ContentRangeHeaderValue(start, end, totalLength);
+
+            // We are now ready to produce partial content.
+            response.StatusCode = HttpStatusCode.PartialContent;
+            response.Content = new PushStreamContent((outputStream, httpContent, transpContext)
+            =>
+            {
+                using (outputStream) // Copy the file to output stream in indicated range.
+                {
+                    using (Stream inputStream = fileInfo.OpenRead())
+                    {
+                        CreatePartialContent(inputStream, outputStream, start, end);
+                    }
+
+                }
+
+            }, GetMimeNameFromExt(fileInfo.Extension));
+
+
+            response.Content.Headers.ContentLength = end - start + 1;
+            response.Content.Headers.ContentRange = contentRange;
+
+            return response;
+        }
+
+
+        #region Others
+
+
+
+        private static MediaTypeHeaderValue GetMimeNameFromExt(string ext)
+        {
+            string value;
+
+            if (MimeNames.TryGetValue(ext.ToLowerInvariant(), out value))
+                return new MediaTypeHeaderValue(value);
+            else
+                return new MediaTypeHeaderValue(MediaTypeNames.Application.Octet);
+        }
+
+        private static bool TryReadRangeItem(RangeItemHeaderValue range, long contentLength,
+            out long start, out long end)
+        {
+            if (range.From != null)
+            {
+                start = range.From.Value;
+                if (range.To != null)
+                    end = range.To.Value;
+                else
+                    end = contentLength - 1;
+            }
+            else
+            {
+                end = contentLength - 1;
+                if (range.To != null)
+                    start = contentLength - range.To.Value;
+                else
+                    start = 0;
+            }
+            return (start < contentLength && end < contentLength);
+        }
+
+        private static void CreatePartialContent(Stream inputStream, Stream outputStream,
+            long start, long end)
+        {
+            int count = 0;
+            long remainingBytes = end - start + 1;
+            long position = start;
+            byte[] buffer = new byte[ReadStreamBufferSize];
+
+            inputStream.Position = start;
+            do
+            {
+                try
+                {
+                    if (remainingBytes > ReadStreamBufferSize)
+                        count = inputStream.Read(buffer, 0, ReadStreamBufferSize);
+                    else
+                        count = inputStream.Read(buffer, 0, (int)remainingBytes);
+                    outputStream.Write(buffer, 0, count);
+                }
+                catch (Exception error)
+                {
+                  //  throw new Exception(error.ToString());
+                }
+                position = inputStream.Position;
+                remainingBytes = end - position + 1;
+            } while (position <= end);
+        }
+
+        #endregion
+
+    }
+}
---
+++ VideoStream/VideoStreamAPI/Controllers/VideosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VideoStreamLib;
+
+namespace VideoStreamAPI.Controllers
+{
+    [RoutePrefix("api/videos")]
+    public class VideosController : ApiController
+    {
+        [Route("play")]
+        [HttpGet]
+        public HttpResponseMessage Play(string f)
+        {
+            return new VideoStream().StreamVideo(Path.Combine(@"C:\F2F\", f), base.Request.Headers.Range, 2);
+        }
+    }
+}
